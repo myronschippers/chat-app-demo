60cda334a5a88f7e2940eeea6dadd0ff8a6378ea
Myron <dubtwee@gmail.com>
2020-08-07 21:45:13 -0500

    adding a wrapping container to the user page content

--- src/components/pages/UserPage/UserPage.js
+++ src/components/pages/UserPage/UserPage.js
@@ -3,17 +3,20 @@ import { connect } from 'react-redux';
 import LogOutButton from '../../LogOutButton/LogOutButton';
 import mapStoreToProps from '../../../redux/mapStoreToProps';
 
+// MATERIAL-UI
+import { Container } from '@material-ui/core';
+
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
 const UserPage = (props) => (
-  <div>
+  <Container>
     <h1 id="welcome">
       Welcome, { props.store.user.username }!
     </h1>
     <p>Your ID is: {props.store.user.id}</p>
     <LogOutButton className="log-in" />
-  </div>
+  </Container>
 );
 
 // this allows us to use <App /> in index.js
