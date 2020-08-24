ff9533ad1b5d696522a64eba72a5e8f10b284ab3
Myron <dubtwee@gmail.com>
2020-08-24 16:26:58 -0500

    adding container and panel classes to the About page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -6,11 +6,9 @@ import React from 'react';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 const AboutPage = () => (
-  <div>
-    <div>
-      <p>
-        This about page is for anyone to read!
-      </p>
+  <div className="container">
+    <div className="panel">
+      <p>This about page is for anyone to read!</p>
     </div>
   </div>
 );
